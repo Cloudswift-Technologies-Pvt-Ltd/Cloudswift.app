5db95ea18db0f57be3ce4b7f87b35dc933c368b0
0xnithin-ai <nithin.engineer.ai@gmail.com>
2026-08-17 12:04:33 +0530

    build errs and deplyment setups

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     formats: ["image/webp", "image/avif"],
     dangerouslyAllowSVG: true,
