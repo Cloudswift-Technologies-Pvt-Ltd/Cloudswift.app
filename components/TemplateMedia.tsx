16c10869d36f7a08797ea133257156c46e2ba138
0xnithin-ai <nithin.engineer.ai@gmail.com>
2026-08-20 16:02:19 +0530

    update images

--- components/TemplateMedia.tsx
+++ components/TemplateMedia.tsx
@@ -69,7 +69,7 @@ export default function TemplateMedia({
         />
       )}
 
-      {!contain && (
+      {!contain && !showPhoto && (
         <div
           className={styles.wash}
           style={{
